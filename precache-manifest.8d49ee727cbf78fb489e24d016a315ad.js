d40daba56821b4e4953d70ee16bc7504b203c0a1
Sunil Kumar Sahu <sunil.sahu@enquero.com>
2019-12-09 13:01:29 +0530

    Updates

---
+++ precache-manifest.8d49ee727cbf78fb489e24d016a315ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525348f282e90d36730c6381d8cc6342",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19873dd2c78e8895f2f8",
+    "url": "/monsters-rolodex/static/css/main.73f7dfd4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19873dd2c78e8895f2f8",
+    "url": "/monsters-rolodex/static/js/main.dfec4e27.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
